6037647383ce2c24c56153a3926ae763e040db9a
PizzaPixel-Dev <pizzapixel.dev@gmail.com>
2025-12-30 15:27:08 -0300

    Add hero section, cards layout, and Supabase integration

---
+++ js/cards.js
@@ -0,0 +1,58 @@
+(() => {
+const SUPABASE_URL = "https://tnbfqakvotmgqbrjhmus.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Db5fiANKUMFWJIA3AhH5bQ_PgXZylG6";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function getMotos() {
+    const { data, error } = await supabase
+    .from("motos")
+    .select("*")
+    .eq("active", true)
+    .order("id", { ascending: false });
+
+    if (error) {
+        console.error("Error cargando motos:", error);
+        return [];
+    }
+
+    return data;
+}
+
+const motosTrack = document.getElementById("motosTrack");
+
+function createMotoCard(moto) {
+    return `
+        <article class="moto-card">
+        <div class="moto-img">
+            <img src="${moto.image}" alt="${moto.brand} ${moto.model}">
+        </div>
+
+        <div class="moto-info">
+            <h3>${moto.brand} ${moto.model}</h3>
+            ${moto.year ? `<p class="moto-year">${moto.year}</p>` : ""}
+            <p class="moto-price">$${moto.price.toLocaleString()}</p>
+        </div>
+        </article>
+        `;
+}
+
+
+async function renderMotos() {
+    const motos = await getMotos();
+
+    motosTrack.innerHTML = motos
+        .map(moto => createMotoCard(moto))
+        .join("");
+    console.log(motos);
+    console.log(motosTrack);
+
+}
+
+renderMotos();
+
+
+})();
